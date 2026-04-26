72c1f25c304049760471565d98063fbc1df31668
MarkAdderly <makuadarloh@gmail.com>
2026-04-25 23:31:21 -0400

    Initial Release

---
+++ UpdateBuildNumber.ps1
@@ -0,0 +1,5 @@
+﻿$buildFilePath = "$PSScriptRoot\BuildNumber.txt"
+$currentBuildNumber = [int](Get-Content $buildFilePath)
+$currentBuildNumber++
+$currentBuildNumber | Set-Content $buildFilePath
+Write-Host "##vso[task.setvariable variable=BuildNumber]$currentBuildNumber"
